0d4919c06e4f85d1086bfb4c01f01f25717d73a3
SamuDev <samudev01@gmail.com>
2024-11-14 13:34:06 -0300

    Deleted default files and more

---
+++ frontend/src/app/(page)/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <>
+            <h1 className="text-3xl text-center">Probando... HOME</h1>
+        </>
+    );
+};
+
+export default HomePage;
